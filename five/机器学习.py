fe63018e0e294840401dcd967d474af71434c641
深蓝PC <mail@ri-co.cn>
2020-05-22 15:22:50 +0800

    实现读取数据和正确答案

--- five/机器学习.py
+++ five/机器学习.py
@@ -1,4 +1,19 @@
 import os
+import numpy as np
+
 #构建数据
-def build_data(dir_name):
-    
+def build_data():
+    dirname="five/trainingDigits/"
+    trains = os.listdir(dirname)
+    for train in trains:
+        index = 0
+        with open(file=dirname+train,mode='r') as f:
+            realnum = int(train[0:1])
+            lines = f.readlines()
+            newline = ''
+            for line in lines:
+                newline = ''.join([newline,line])
+            print(newline)
+
+
+build_data()
