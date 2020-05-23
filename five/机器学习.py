4243ae4def735558cceda5dda1a5f344d578480a
深蓝PC <mail@ri-co.cn>
2020-05-23 08:23:23 +0800

    pip前

--- five/机器学习.py
+++ five/机器学习.py
@@ -1,10 +1,12 @@
 import os
 import numpy as np
-
+import matplotlib as plt
+from sklearn.neighbors import KNeighborsClassifier
 #构建数据
 def build_data():
     dirname="five/trainingDigits/"
     trains = os.listdir(dirname)
+    newlines=np.zeros(32,32)
     for train in trains:
         index = 0
         with open(file=dirname+train,mode='r') as f:
@@ -13,7 +15,22 @@ def build_data():
             newline = ''
             for line in lines:
                 newline = ''.join([newline,line])
-            print(newline)
+            newlines[index] = newline
+            index+=1
+    nparray = np.array(newlines)
 
+def save_data(datalist):
+    pass
+
+def knndo():
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit()
+
+#可视化数据
+def view():
+    #创建画布
+    plt.figure()
+    #绘图
+    
 
 build_data()
