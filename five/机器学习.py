42b35a06d175006cb6166f360655df032f2594ab
深蓝PC <mail@ri-co.cn>
2020-05-23 09:18:56 +0800

    数据的存储，读取

--- five/机器学习.py
+++ five/机器学习.py
@@ -1,27 +1,37 @@
 import os
 import numpy as np
 import matplotlib as plt
-from sklearn.neighbors import KNeighborsClassifier
+import sklearn
 #构建数据
 def build_data():
     dirname="five/trainingDigits/"
     trains = os.listdir(dirname)
-    newlines=np.zeros(32,32)
+    #放所有数据的大列表
+    datalist = np.zeros((len(trains),1025))
+    newlines=np.zeros((32,32))
     for train in trains:
-        index = 0
+        fileindex = 0
         with open(file=dirname+train,mode='r') as f:
+            lineindex=0
             realnum = int(train[0:1])
             lines = f.readlines()
-            newline = ''
             for line in lines:
-                newline = ''.join([newline,line])
-            newlines[index] = newline
-            index+=1
-    nparray = np.array(newlines)
+                intline = np.array([int(i) for i in line[0:32]])
+                newlines[lineindex] = intline
+                lineindex+=1
+            datalist[fileindex,0:1] = realnum
+        datalist[fileindex,1:] = newlines.ravel()
+        fileindex+=1
+    save_data(datalist)
+    print()
+        
 
 def save_data(datalist):
-    pass
+    np.save('five/data/',datalist)
 
+def load_data():
+    train_data = np.load('five/data/train.npy')
+    print(train_data)
 def knndo():
     knn = KNeighborsClassifier(n_neighbors=5)
     knn.fit()
@@ -33,4 +43,4 @@ def view():
     #绘图
     
 
-build_data()
+load_data()
